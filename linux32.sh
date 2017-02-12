fd219fdb38d03d645628d16b77e5d36741368a12
Nguyen Tien Dung <ngtdungnt@gmail.com>
2017-02-12 18:04:22 +0700

    fix linux build

---
+++ linux32.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y software-properties-common
+apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
+apt-get -yq update
+apt-get -yq --no-install-suggests --no-install-recommends --force-yes install libgnome-keyring-dev
+apt-get install -y curl gcc make libfontconfig-dev
+
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+. ~/.nvm/nvm.sh
+
+nvm install 6
+cd /fm
+
+rm -rf ./node_modules
+rm -rf ./app/node_modules
+
+nvm ls
+
+node -v
+npm -v
+
+npm install
+
+tar -cvzf ./dist/fontmanager-1.4.14-linux-ia32.tar.gz -C ./app/node_modules/font-manager/build/Release fontmanager.node
+
